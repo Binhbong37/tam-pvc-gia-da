0727744dc65ecd0518df69bbdb5b6d0cca16aa87
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-09-02 20:55:06 +0700

    file register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Register = () => {
   console.log('Resgitster');
   console.log(process.env);
-  console.log(process.env.REACT_APP);
   return (
     <div>Register</div>
   )
